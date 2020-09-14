7b4c71e6791448b4ce78f78ea866203c5de9206c
Moa1er <twoswaglol.law@gmail.com>
2020-09-14 09:35:36 -0400

    fin logique du programme

--- updateOnFlights.py
+++ updateOnFlights.py
@@ -5,6 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 
+from bs4 import BeautifulSoup # BeautifulSoup is in bs4 package 
+import requests
+
 import pandas as pd
 
 import time
@@ -21,19 +24,57 @@ classDisabled = "datepicker-day-button day disabled"
 browser = webdriver.Chrome(executable_path='/home/miameme/Downloads/chromedriver')
 browser.get(url)
 
+content = requests.get(url)
+soup = BeautifulSoup(content.text, 'html.parser')
 
-def triggersTheMenu():
+def triggerMenuDestination():
     browser.find_element_by_xpath("//app-flight-input[@class='searcher-input destination separator']").click()
 
 def arrival_country_chooser(arrival_country):
-    fly_to = browser.find_element_by_xpath("//input[@id='inputDestination']").clear()
+    fly_to = browser.find_element_by_xpath("//input[@id='inputDestination']")
+    fly_to.clear()
     time.sleep(0.5)
     fly_to.send_keys(arrival_country)
     time.sleep(0.5)
     browser.find_element_by_xpath("//app-station-item[@class='item list-station']").click() #Choosing the first item
+    time.sleep(0.5)
 
-#def choosingDate() :
-    
+def triggerMenuDate():
+    browser.find_element_by_xpath("//app-flight-date-input[@class='searcher-input']").click()
+    time.sleep(0.5)
 
-triggersTheMenu()
-arrival_country_chooser("Paris")
+def triggerNextMonths():
+    browser.find_element_by_xpath("//span[@class='icon-chevron-right arrowforward']").click()
+    time.sleep(0.5)
+    browser.find_element_by_xpath("//span[@class='icon-chevron-right arrowforward']").click()
+    time.sleep(0.5)
+
+def isAvailable():
+    available = browser.find_elements_by_xpath("//span[@class='datepicker-day-button day disabled']")
+    if len(available) < 62 :
+        sendEmail("ALERTE ROUGE, ALERTE ROUGE, ceci n'est pas un exercice.")
+    else :
+        sendEmail("ALERTE... GREEN LOL")
+
+def connectMail():
+    global server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login("aghines.gasselin@gmail.com", "Ag-elpatro1")
+
+def sendEmail(etatVol) :
+    global message
+    message = MIMEMultipart()
+    message['Subject'] = 'Update availability of level\'s flights'
+    message['From'] = 'aghines.gasselin@gmail.com'
+    message['to'] = 'twoswaglol.law@gmail.com'
+
+    server.sendmail('aghines.gasselin@gmail.com', 'twoswaglol.law@gmail.com', etatVol)
+
+triggerMenuDestination()           #OK
+arrival_country_chooser("Paris")   #OK
+triggerMenuDate()                  #OK
+triggerNextMonths()                #OK en full screen...
+connectMail()                      #OK
+isAvailable()                      #OK mais bon pas tres clean on va pas se le cacher
